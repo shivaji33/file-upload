2e881b72188b8e952938f75e1dbe1a7a9fc1b105
shivaji33 <naidupilla84@gmail.com>
2021-02-21 17:40:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wu4nl5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wu4nl5)
---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.scss']
+})
+export class FileUploadComponent implements OnInit {
+message: string;
+imagePath: any;
+imgURL: string|ArrayBuffer;
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+  }
+preview(files) {
+    if (files.length === 0)
+      return;
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    const reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+}
