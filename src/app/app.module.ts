92e095dc1fca03dfedf572c705db6d71a82cfc78
shivaji33 <naidupilla84@gmail.com>
2021-02-21 17:49:24 +0530

    3 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FileUploadComponent ],
+  declarations: [ AppComponent, FileUploadComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+0
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,35 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-file-upload',
-  templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss']
+  selector: "app-file-upload",
+  templateUrl: "./file-upload.component.html",
+  styleUrls: ["./file-upload.component.scss"]
 })
 export class FileUploadComponent implements OnInit {
-message: string;
-imagePath: any;
-imgURL: string|ArrayBuffer;
+  message: string;
+  imagePath: any;
+  imgURL: string | ArrayBuffer;
 
-  constructor() {
-    
-   }
+  constructor() {}
 
-  ngOnInit() {
-  }
-preview(files) {
-    if (files.length === 0)
-      return;
+  ngOnInit() {}
+  preview(files) {
+    if (files.length === 0) return;
     const mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
       this.message = "Only images are supported.";
       return;
     }
- 
+
     const reader = new FileReader();
     this.imagePath = files;
-    reader.readAsDataURL(files[0]); 
-    reader.onload = (_event) => { 
-      this.imgURL = reader.result; 
-    }
+    reader.readAsDataURL(files[0]);
+    reader.onload = _event => {
+      this.imgURL = reader.result;
+    };
   }
 }
